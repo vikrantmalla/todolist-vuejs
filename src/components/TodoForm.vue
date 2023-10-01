a85f3c2bfb03723da2e75b1edf73dec23c2931e3
bikrant malla <bikrant.malla@truenary.com>
2023-10-01 21:03:21 +0545

    feat: add a input field

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="addTask">
+    <div className="flex flex-row justify-center items-start gap-4">
+      <div>
+        <input
+          v-model="newTask"
+          className="p-2 w-[100%] border"
+          type="text"
+          placeholder="Enter a new task..."
+        />
+      </div>
+      <button className="py-2 px-4 border" @click="addTask">+</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        this.tasks.push(this.newTask); // Add the task to the tasks array
+        this.newTask = ""; // Reset the input field
+      }
+    },
+  },
+};
+</script>
+
+
+
+
+
