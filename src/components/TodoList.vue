67f628c3e559c183946f3502f98cab533ae8f577
bikrant malla <bikrant.malla@truenary.com>
2023-10-02 21:48:43 +0545

    feat: add edit and delete btn

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,6 +3,13 @@ import { ref, defineProps } from "vue";
 const { tasks } = defineProps({
   tasks: Array,
 });
+console.log("beforeUpdate", tasks);
+const handleTaskRemoval = (taskId) => {
+  const updatedTasks = tasks.filter((task) => task.id !== taskId);
+  tasks = updatedTasks;
+  console.log(updatedTasks);
+  console.log("afterUpdate", tasks);
+};
 </script>
 
 <template>
@@ -14,6 +21,10 @@ const { tasks } = defineProps({
         :key="index"
       >
         {{ task }}
+        <div className="flex gap-4">
+          <button @click="handleEditTask(index)">Edit</button>
+          <button @click="handleTaskRemoval(task.id)">Delete</button>
+        </div>
       </li>
     </ul>
   </div>
